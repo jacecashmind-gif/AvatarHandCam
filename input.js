856aaaaaf60a91c78706de72d4fc69ccf87e3c3e
jacecashmind-gif <jacecashmind@gmail.com>
2026-06-28 04:31:13 -0700

    Create input.js

---
+++ input.js
@@ -0,0 +1,11 @@
+const keys = {};
+
+window.addEventListener("keydown", (e) => {
+    keys[e.code] = true;
+});
+
+window.addEventListener("keyup", (e) => {
+    keys[e.code] = false;
+});
+
+export { keys };
